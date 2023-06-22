483f9604fa132af6ab33d65598c4371de8a926a4
bar07865 <137136679+bar07865@users.noreply.github.com>
2023-06-22 06:38:38 +0000

    print lines

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def main():
     # make your code below
     # print('Hello World')
     ##############################
+    print('Hello World')
+    print('Pythong Programming')
 
 
 if __name__ == '__main__':
